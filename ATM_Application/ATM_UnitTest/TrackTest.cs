4ade001dc6282e1f59a539d2cc5229476b67074e
au551828 <OdenseMafia@Hotmail.com>
2018-04-10 00:29:39 +0200

    UT

---
+++ ATM_Application/ATM_UnitTest/TrackTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace ATM_UnitTest
+{
+    [TestFixture]
+    class TrackTest
+    {
+        private string _tag;
+        private string _x;
+        private string _y;
+        private string _altitude;
+        private string _timeStamp;
+
+        
+
+
+    }
+}
