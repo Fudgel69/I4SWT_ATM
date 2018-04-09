9590299c6b2d307a204ec88d1f8a9e1f004b6cd3
au551828 <OdenseMafia@Hotmail.com>
2018-04-09 23:36:10 +0200

    Test videre

--- ATM_Application/ATM_UnitTest/TimeTest.cs
+++ ATM_Application/ATM_UnitTest/TimeTest.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATM_Class;
+using NUnit.Framework;
 
 namespace ATM_UnitTest
 {
+    [TestFixture]
     class TimeTest
     {
+        private 
+
+
+        [SetUp]
+        public void Setup()
+        {
+            year_ = new Time("0");
+
+        }
+
+        [Test]
+        public void Year_TooManyCharacters()
+        {
+            
+
+        }
+
+
+
+
     }
 }
