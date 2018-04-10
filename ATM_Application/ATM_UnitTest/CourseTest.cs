ce561df9963bf1adf862878dac56f3ca563b91a6
Sevelboen <tobias.saaby@live.com>
2018-04-10 10:25:03 +0200

    Unit test for Course

---
+++ ATM_Application/ATM_UnitTest/CourseTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ATM_Class;
+
+namespace ATM_UnitTest
+{
+    [TestFixture]
+    class CourseTest
+    {
+        Course CurCourse = new Course();
+        Position CurPos = new Position();
+        Position OldPos = new Position();
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+
+        [Test]
+        public void TestCalculateCourseX()
+        {
+            CurPos.SetPosition(20000, 20000, 10000);
+            OldPos.SetPosition(10000, 20000, 10000);
+
+            double expectedCourse = 90;
+            CurCourse.CalculateCourse(CurPos, OldPos);
+            Assert.That(CurCourse._course, Is.EqualTo(expectedCourse));
+
+        }
+
+        [Test]
+        public void TestCalculateCourseY()
+        {
+            CurPos.SetPosition(20000, 20000, 10000);
+            OldPos.SetPosition(20000, 10000, 10000);
+
+            double expectedCourse = 0;
+            CurCourse.CalculateCourse(CurPos, OldPos);
+            Assert.That(CurCourse._course, Is.EqualTo(expectedCourse));
+
+        }
+
+        [Test]
+        public void TestCalculateCourseXY()
+        {
+            CurPos.SetPosition(20000, 20000, 10000);
+            OldPos.SetPosition(10000, 10000, 10000);
+
+            double expectedCourse = 45;
+            CurCourse.CalculateCourse(CurPos, OldPos);
+            Assert.That(CurCourse._course, Is.EqualTo(expectedCourse));
+
+        }
+
+
+    }
+}
