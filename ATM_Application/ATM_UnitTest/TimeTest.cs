10b7ac1cc8d62e7670359bf44b8c764fa0088cca
Sevelboen <tobias.saaby@live.com>
2018-04-10 09:13:55 +0200

    Time Unit-test lavet

--- ATM_Application/ATM_UnitTest/TimeTest.cs
+++ ATM_Application/ATM_UnitTest/TimeTest.cs
@@ -12,21 +12,27 @@ namespace ATM_UnitTest
     [TestFixture]
     class TimeTest
     {
-        private Time t;
-        private ITime _time;
-        
+        private int _day;
+        private int _hour;
+        private int _second;
+        private int _millisecond;
+
+        // private Time t;
+        // private ITime _time;
+
+        private Time t = new Time("20181004085100000");
+        private Time _time = new Time("20181004095100000");
+
         [SetUp]
         public void Setup()
         {
-            _time.TimeDifferenceSec(t);
+
         }
 
         [Test]
         public void TestTime()
         {
-
+            Assert.That(_time.TimeDifferenceSec(t), Is.EqualTo(3600));
         }
-
-
     }
 }
