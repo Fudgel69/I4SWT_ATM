4dc939925b7e1541ea98c6896052808e40145303
Don Bui <marcauphongbui@gmail.com>
2018-04-20 11:00:42 +0200

    Added NSubstitute and started on IT step 1

--- ATM_Application/ATM_IntegrationTest/IT1_TrackToPosition.cs
+++ ATM_Application/ATM_IntegrationTest/IT1_TrackToPosition.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using ATM_Class;
+using ATM_Class.Classes;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace ATM_IntegrationTest
 {
+    [TestFixture]
     public class IT1_TrackToPosition
     {
+        private IPosition _position;
+        private ITime _time;
+        private ITrack _track;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _position = new Position();
+            _time = Substitute.For<ITime>();
+            _track = new Track();
+        }
+
+        [Test]
+        public void 
     }
 }
