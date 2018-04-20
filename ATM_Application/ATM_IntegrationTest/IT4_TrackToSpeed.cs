95a0abefb1cf5c7ff231310312f7a1197380b79a
simo817b <31984429+simo817b@users.noreply.github.com>
2018-04-20 12:29:41 +0200

    Første udkast

--- ATM_Application/ATM_IntegrationTest/IT4_TrackToSpeed.cs
+++ ATM_Application/ATM_IntegrationTest/IT4_TrackToSpeed.cs
@@ -1,20 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ATM_Class;
+﻿using ATM_Class;
 using NSubstitute;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
+
 
 namespace ATM_IntegrationTest
 {
-    [TextFixture]
+    [TestFixture]
     public class IT4_TrackToSpeed
     {
-        private ITrack _track;
-        private ITime _time;
+        private IPosition _newPos = new Position();
+        private IPosition _oldPos = new Position();
+        private ITime _oldtime;
+        private ITime _newtime;
         private ISpeed _speed;
-        private ICourse _course;
+        private ITrack _track;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _newPos.SetPosition(56000, 20000, 10000);
+            _oldPos.SetPosition(20000, 20000, 10000);
+            _newtime = new Time("20181004095100000");
+            _oldtime = new Time("20181004085100000");
+            _track = new Track("Flight", _oldPos, _oldtime);
+
+        }
+        [Test]
+        public void TestSpeedCorrect()
+        {
+            double expectedSpeed = 10.00;
+            _track.UpdateTrack("Flight", _newPos, _oldtime);
+            Assert.That(_track.CurrentSpeed, Is.EqualTo(expectedSpeed));
+        }
+
     }
 }
