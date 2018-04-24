e7e310e22394b8e88d56a83352eb60856c512e28
Sevelboen <tobias.saaby@live.com>
2018-04-24 14:10:35 +0200

    Det gjorde det alligevel ikke :D IT5 skulle gerne være done!

--- ATM_Application/ATM_IntegrationTest/IT5_AirportToTrack.cs
+++ ATM_Application/ATM_IntegrationTest/IT5_AirportToTrack.cs
@@ -17,7 +17,7 @@ namespace ATM_IntegrationTest
         private Time _time;
         private ISpeed _speed;
         private Track _trackOne;
-        private ITrack _trackTwo;
+        private Track _trackTwo;
         private ITransponderReceiver _receiver;
         private AirspaceMonitor _airspaceMonitor;
 
@@ -27,6 +27,7 @@ namespace ATM_IntegrationTest
             _pos.SetPosition(20000, 20000, 10000);
             _time = new Time("20181010105111111");
             _trackOne = new Track("ABC123", _pos, _time);
+            _trackTwo = new Track("XYZ789", _pos, _time);
 
             _receiver = Substitute.For<ITransponderReceiver>();
             _airspaceMonitor = new AirspaceMonitor(_receiver);
@@ -46,6 +47,23 @@ namespace ATM_IntegrationTest
             _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(TRACK));
 
             Assert.That(_airspaceMonitor.Tracks[0].Tag, Is.EqualTo(_trackOne.Tag));
+        }
+
+        [Test]
+        public void TwoNewTracks()
+        {
+            var TRACK = new List<string>
+            {
+                $"{_trackOne.Tag};{_trackOne.CurrentPosition.X};{_trackOne.CurrentPosition.Y};{_trackOne.CurrentPosition.Altitude};{_trackOne.CurrentTime.Year}{_trackOne.CurrentTime.Month}{_trackOne.CurrentTime.Day}{_trackOne.CurrentTime.Hour}{_trackOne.CurrentTime.Minute}{_trackOne.CurrentTime.Second}{_trackOne.CurrentTime.MilliSecond}",
+                $"{_trackTwo.Tag};{_trackTwo.CurrentPosition.X};{_trackTwo.CurrentPosition.Y};{_trackTwo.CurrentPosition.Altitude};{_trackTwo.CurrentTime.Year}{_trackTwo.CurrentTime.Month}{_trackTwo.CurrentTime.Day}{_trackTwo.CurrentTime.Hour}{_trackTwo.CurrentTime.Minute}{_trackTwo.CurrentTime.Second}{_trackTwo.CurrentTime.MilliSecond}"
+            };
+
+
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(TRACK));
+
+            Assert.That(_airspaceMonitor.Tracks.Count, Is.EqualTo(2));
+            Assert.That(_airspaceMonitor.Tracks[0].Tag, Is.EqualTo(_trackOne.Tag));
+            Assert.That(_airspaceMonitor.Tracks[1].Tag, Is.EqualTo(_trackTwo.Tag));
         }
 
         [Test]
@@ -93,6 +111,31 @@ namespace ATM_IntegrationTest
 
             Assert.That(_airspaceMonitor.Tracks[0].CurrentCourse._course, Is.EqualTo(_trackOne.CurrentCourse._course));
         }
+
+        [Test]
+        public void UpdateTrackMakesSpeed()
+        {
+            var TRACK = new List<string>
+            {
+                $"{_trackOne.Tag};{_trackOne.CurrentPosition.X};{_trackOne.CurrentPosition.Y};{_trackOne.CurrentPosition.Altitude};{_trackOne.CurrentTime.Year}{_trackOne.CurrentTime.Month}{_trackOne.CurrentTime.Day}{_trackOne.CurrentTime.Hour}{_trackOne.CurrentTime.Minute}{_trackOne.CurrentTime.Second}{_trackOne.CurrentTime.MilliSecond}"
+            };
+
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(TRACK));
+            _time = new Time("20181010115111111");
+            _pos.SetPosition(10000, 10000, 10000);
+            _trackOne.UpdateTrack("ABC123", _pos, _time);
+            
+            TRACK = new List<string>
+            {
+                $"{_trackOne.Tag};{_trackOne.CurrentPosition.X};{_trackOne.CurrentPosition.Y};{_trackOne.CurrentPosition.Altitude};{_trackOne.CurrentTime.Year}{_trackOne.CurrentTime.Month}{_trackOne.CurrentTime.Day}{_trackOne.CurrentTime.Hour}{_trackOne.CurrentTime.Minute}{_trackOne.CurrentTime.Second}{_trackOne.CurrentTime.MilliSecond}"
+            };
+
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(TRACK));
+
+            Assert.That(_airspaceMonitor.Tracks[0].CurrentSpeed._speed, Is.EqualTo(_trackOne.CurrentSpeed._speed));
+        }
+
+
 
     }
 }
