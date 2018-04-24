3b014b6e25af954322ee16787ff6c1c59d41cd2f
Sevelboen <tobias.saaby@live.com>
2018-04-24 13:08:25 +0200

    IT3 done

--- ATM_Application/ATM_IntegrationTest/IT3_TrackToTime.cs
+++ ATM_Application/ATM_IntegrationTest/IT3_TrackToTime.cs
@@ -1,7 +1,66 @@
-﻿namespace ATM_IntegrationTest
+﻿using ATM_Class;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ATM_IntegrationTest
 {
     public class IT3_TrackToTime
     {
-        
+
+        private Position _position;
+        private Time _time;
+        private Track _track;
+        private Course _course;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _position = new Position();
+            _position.X = 20000;
+            _position.Y = 20000;
+            _position.Altitude = 5000;
+            _course = new Course();
+            _time = new Time("20181004095100000");
+            _track = new Track("Flight", _position, _time);
+        }
+
+        [Test]
+        public void TestTimeInTrack()
+        {
+
+            Assert.That(_track.CurrentTime.Year, Is.EqualTo(_time.Year));
+            Assert.That(_track.CurrentTime.Month, Is.EqualTo(_time.Month));
+            Assert.That(_track.CurrentTime.Day, Is.EqualTo(_time.Day));
+            Assert.That(_track.CurrentTime.Hour, Is.EqualTo(_time.Hour));
+            Assert.That(_track.CurrentTime.Minute, Is.EqualTo(_time.Minute));
+            Assert.That(_track.CurrentTime.Second, Is.EqualTo(_time.Second));
+            Assert.That(_track.CurrentTime.MilliSecond, Is.EqualTo(_time.MilliSecond));
+
+        }
+
+        [Test]
+        public void TestTimeInTrackUpdated()
+        {
+            Time _timetwo = new Time("20191105085100000");
+            _track.UpdateTrack("Flight", _position, _timetwo);
+            Assert.That(_track.CurrentTime.Year, Is.EqualTo(_timetwo.Year));
+            Assert.That(_track.CurrentTime.Month, Is.EqualTo(_timetwo.Month));
+            Assert.That(_track.CurrentTime.Day, Is.EqualTo(_timetwo.Day));
+            Assert.That(_track.CurrentTime.Hour, Is.EqualTo(_timetwo.Hour));
+            Assert.That(_track.CurrentTime.Minute, Is.EqualTo(_timetwo.Minute));
+            Assert.That(_track.CurrentTime.Second, Is.EqualTo(_timetwo.Second));
+            Assert.That(_track.CurrentTime.MilliSecond, Is.EqualTo(_timetwo.MilliSecond));
+
+        }
+
+        [Test]
+        public void TestTimeInTrackDayPassed()
+        {
+            Time _timetwo = new Time("20181005095100000");
+            _track.UpdateTrack("Flight", _position, _timetwo);
+            Assert.That(_track.CurrentTime.TimeDifferenceSec(_track.OldTime), Is.EqualTo(86400));
+
+        }
+
     }
 }
