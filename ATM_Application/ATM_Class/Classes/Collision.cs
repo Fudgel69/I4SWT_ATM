4079f5f12948e7df36da3097226658d1ddeb47b0
Don Bui <marcauphongbui@gmail.com>
2018-04-24 18:51:47 +0200

    Implementation that is incomplete with LINQ

--- ATM_Application/ATM_Class/Classes/Collision.cs
+++ ATM_Application/ATM_Class/Classes/Collision.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ATM_Class.Classes
+namespace ATM_Class
 {
-    class Collision
+    public class Collision : ICollision
     {
+        public DateTime TimeOccurence { get; }
+        public List<string> CollisionTag { get; } = new List<string>();
+
+
+        //constructor
+        public Collision(string s1, string s2, DateTime time)
+        {
+            CollisionTag.Add(s1);
+            CollisionTag.Add(s2);
+            TimeOccurence = time;
+        }
+
+        /*
+        public override string ToString()
+        {
+            
+        }
+        */
     }
 }
