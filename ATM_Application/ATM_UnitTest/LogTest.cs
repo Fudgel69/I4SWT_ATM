cd4007165eefa7a18b204da20dfd43590f8961ef
Sevelboen <tobias.saaby@live.com>
2018-05-16 00:47:25 +0200

    Ny LogTest der logger et indkommende event

--- ATM_Application/ATM_UnitTest/LogTest.cs
+++ ATM_Application/ATM_UnitTest/LogTest.cs
@@ -8,23 +8,31 @@ using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using ATM_Class;
+using NSubstitute;
+using TransponderReceiver;
 
 namespace ATM_UnitTest
 {
     [TestFixture]
     public class LogTest
     {
+        private SepEventsLogger uut;
+        private INewSepEvent testEvent;
+        private ITrack T1, T2;
 
         [SetUp]
         public void SetUp()
         {
-           
+            testEvent = Substitute.For<INewSepEvent>();
+            T1 = Substitute.For<ITrack>();
+            T2 = Substitute.For<ITrack>();
+            uut = new SepEventsLogger(testEvent);
         }
 
         [Test]
         public void WriteFileDoesExist()
         {
-            var uut = new SepEventsLogger();
+
             uut.Log("ABC123", "DEF456");
 
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\" + "SepEventsLog.txt";
@@ -36,16 +44,30 @@ namespace ATM_UnitTest
         [Test]
         public void WriteFileDoesNotExist()
         {
+
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\" + "SepEventsLog.txt";
             File.Delete(path);
             Assert.IsFalse(File.Exists(path));
 
-            var uut = new SepEventsLogger();
             uut.Log("ABC123", "DEF456");
 
             Assert.IsTrue(File.Exists(path));
 
+        }
 
+        [Test]
+        public void LogNewEvent()
+        {
+            T1.Tag.Returns("T1");
+            T2.Tag.Returns("T2");
+
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\" + "SepEventsLog.txt";
+            File.Delete(path);
+            Assert.IsFalse(File.Exists(path));
+
+            testEvent.CrashingEvent += Raise.EventWith(new SeperationEventArgs(T1, T2));
+            
+            Assert.IsTrue(File.Exists(path));
 
         }
     }
